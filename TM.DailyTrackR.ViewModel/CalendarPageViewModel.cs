99795339fe3c74c30d3aa4edfcc64f6b53675da0
tincudiana-carla <carla_diana@Yahoo.com>
2024-06-19 22:09:28 +0300

    1.Adding the 2 tabs(DailyWork and Overview) 2. By clicking a date from the calendar,  the textblock will change its text with the date(String format).

--- TM.DailyTrackR.ViewModel/CalendarPageViewModel.cs
+++ TM.DailyTrackR.ViewModel/CalendarPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,33 @@ namespace TM.DailyTrackR.ViewModel
 {
     public class CalendarPageViewModel : BindableBase
     {
+
+        private DateTime selectedDate;
+        public DateTime SelectedDate
+        {
+            get => selectedDate;
+            set
+            {
+                SetProperty(ref selectedDate, value);
+                UpdateActivitiesDate(value); 
+            }
+        }
+
+        private string activitiesDateText;
+        public string ActivitiesDateText
+        {
+            get => activitiesDateText;
+            set => SetProperty(ref activitiesDateText, value);
+        }
+
         public CalendarPageViewModel()
         {
-            
+            ActivitiesDateText = "Activities Date: ";
+        }
+
+        private void UpdateActivitiesDate(DateTime selectedDate)
+        {
+            ActivitiesDateText = $"Activities Date: {selectedDate.ToString("dd/MM/yyyy")}";
         }
     }
 }
